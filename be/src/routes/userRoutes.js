a74ee942de951a6482af68ee2fe7f8973d1b552c
maivankien <maivankien1710@gmail.com>
2023-02-06 17:20:48 +0700

    get user product

--- be/src/routes/userRoutes.js
+++ be/src/routes/userRoutes.js
@@ -2,11 +2,15 @@ const express = require('express')
 const router = express.Router()
 
 const userController = require('../controllers/userController')
+const productController = require('../controllers/productController')
 
 router.post('/register', userController.userRegister)
 router.post('/login', userController.userLogin)
 router.post('/refreshToken', userController.requestRefreshToken)
 router.post('/logout', userController.userLogout)
+
+router.get('/getProduct/:id', productController.getProduct)
+router.get('/getAllProduct', productController.getAllProduct)
 
 
 module.exports = router
