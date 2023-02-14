7889b4459e2557ab5a4dcd83e3f9dfd8936c1dad
maivankien <maivankien1710@gmail.com>
2023-02-14 10:06:41 +0700

    register fe

---
+++ fe/pages/login/assets/js/login.js
@@ -0,0 +1,39 @@
+const message = document.querySelector('.message-error')
+
+document.querySelector('.form-login').addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = {};
+    formData.forEach((value, key) => {
+        data[key] = value;
+    })
+    try {
+        let result = await fetch('http://localhost:3000/api/user/login', {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        const response = await result.json()
+        if (response.EC = -1 && typeof response.message != 'object') {
+            message.textContent = response.message
+        }
+        if (response.message == 'Successful') {
+            // window.location.href = '../login/login.html'
+            function getCookie(name) {
+                const value = `; ${document.cookie}`;
+                const parts = value.split(`; ${name}=`);
+                if (parts.length === 2) {
+                  return parts.pop().split(';').shift();
+                }
+              }
+              const accessToken = getCookie('accessToken');
+              const refreshToken = getCookie('refreshToken');
+              console.log(accessToken, refreshToken)
+            //   console.log()
+        }
+    } catch (error) {
+        console.error(error)
+    }
+})
--- fe/pages/register/assets/js/register.js
+++ fe/pages/register/assets/js/register.js
@@ -1,29 +1,28 @@
+const message = document.querySelector('.message-error')
 
-
-document.querySelector('.form-register').addEventListener('submit', function (event) {
+document.querySelector('.form-register').addEventListener('submit', async (event) => {
     event.preventDefault();
-
     const formData = new FormData(event.target);
     const data = {};
-    formData.forEach(function (value, key) {
+    formData.forEach((value, key) => {
         data[key] = value;
-    });
-    console.log(data)
-
-    fetch('https://maivankien-clothing.onrender.com/api/user/register', {
-        method: 'POST',
-        body: JSON.stringify(data),
-        headers: {
-            'Content-Type': 'application/json'
-        }
     })
-        .then(function (response) {
-            return response.json();
+    try {
+        let result = await fetch('https://maivankien-clothing.onrender.com/api/user/register', {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json'
+            }
         })
-        .then(function (data) {
-            console.log(data);
-        })
-        .catch(function (error) {
-            console.error(error);
-        });
-});
+        const response = await result.json()
+        if (response.EC = -1 && typeof response.message != 'object') {
+            message.textContent = response.message
+        }
+        if (response.message == 'Account successfully created') {
+            window.location.href = '../login/login.html'
+        }
+    } catch (error) {
+        console.error(error)
+    }
+})
